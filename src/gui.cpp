4c21582f4b82366c737186a9da272203edf0c1d2
Piotr J. Tomaszewski <pjtomaszewski98@gmail.com>
2021-09-22 18:43:04 +0200

    Change window name and add GUI cleanup

--- src/gui.cpp
+++ src/gui.cpp
@@ -22,7 +22,7 @@ GUI::GUI(System &system): system(system), display_renderer(system.get_io().displ
     SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
     SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
     SDL_WindowFlags window_flags = (SDL_WindowFlags)(SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI);
-    window = SDL_CreateWindow("GameBoy Emulator", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, window_flags);
+    window = SDL_CreateWindow("Chip8", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, window_flags);
     gl_context = SDL_GL_CreateContext(window);
     SDL_GL_MakeCurrent(window, gl_context);
     SDL_GL_SetSwapInterval(1); // Enable vsync
@@ -47,7 +47,14 @@ GUI::GUI(System &system): system(system), display_renderer(system.get_io().displ
 }
 
 GUI::~GUI() {
+    // Cleanup
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplSDL2_Shutdown();
+    ImGui::DestroyContext();
 
+    SDL_GL_DeleteContext(gl_context);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 }
 
 void GUI::display() {
